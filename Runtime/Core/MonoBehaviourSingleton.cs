9e2188323a2bc809de6b1126e236c2872232af78
WonSeokSong <songwon4529@naver.com>
2025-12-05 13:55:51 +0900

    [Feat] 싱글톤 추가

---
+++ Runtime/Core/MonoBehaviourSingleton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace SongLib.Core
+{
+    /// <summary>
+    /// Generic MonoBehaviour Singleton.
+    /// Automatically creates an instance if none exists.
+    /// </summary>
+    public abstract class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviourSingleton<T>
+    {
+        private static T _instance;
+        private static readonly object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        _instance = FindObjectOfType<T>();
+                        if (_instance == null)
+                        {
+                            var obj = new GameObject(typeof(T).Name);
+                            _instance = obj.AddComponent<T>();
+                            DontDestroyOnLoad(obj);
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = (T)this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
